00f52fe046a6965dd896f32565333c3869f0dcbd
Kirill.Znamenskiy.pw <Kirill@Znamenskiy.pw>
2020-08-29 00:17:04 +0200

    +

--- inself-remote-upload-devel.php
+++ inself-remote-upload-devel.php
@@ -1,7 +1,7 @@
 <?php
 require(__DIR__.'/vendor/autoload.php');
 
-$remote_project_dir_path = '/home/vv/dev.sendpass.online;';
+$remote_project_dir_path = '/home/vv/dev.sendpass.online';
 $remote_branch_name = 'develop';
 require(__DIR__.'/inself-remote-upload-master.php');
 
