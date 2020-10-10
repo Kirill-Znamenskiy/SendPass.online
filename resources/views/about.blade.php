234f90598560348fb9628b2f1371304931be9991
Kirill.Znamenskiy.pw <Kirill@Znamenskiy.pw>
2020-10-10 18:24:07 +0200

    +

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -22,22 +22,7 @@
                     <h1 class="card-header h4">{{__('about.about_project', ['app_name' => $app_name, 'app_url' => $app_url, 'app_link' => $app_link])}}</h1>
                     <div class="card-body">
 
-                        {{--            <h3 class="h5">Collapsible Group Item #1 <small class="text-muted">With faded secondary text</small></h3>--}}
-                        {{--            <p>--}}
-                        {{--                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.--}}
-                        {{--            </p>--}}
-
-
-                        @for ($i = 1; $i < 9; $i++)
-
-                            @break(!(app('translator')->has('faq.question'.$i)))
-
-                            @if ($i > 1) <hr/> @endif
-
-                            <h3 class="h5">@lang('faq.question'.$i, ['app_name' => $app_name, 'app_url' => $app_url, 'app_link' => $app_link])</h3>
-                            <p>@lang('faq.answer'.$i, ['app_name' => $app_name, 'app_url' => $app_url, 'app_link' => $app_link])</p>
-                        @endfor
-
+                        {{__('about.main_content', ['app_name' => $app_name, 'app_url' => $app_url, 'app_link' => $app_link])}}
 
 
                     </div>
