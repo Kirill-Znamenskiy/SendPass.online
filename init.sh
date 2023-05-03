c82e302761b800960ee89c21200cb40f0dd16036
Kirill.Znamenskiy.pw <Kirill@Znamenskiy.pw>
2023-05-02 23:42:51 -0300

    +++

---
+++ init.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -xe
+
+if test ! -f "./init.bash"; then
+    echo "Work directory '$PWD' is wrong!"
+    exit
+fi
+
+mkdir -p ./runtime
+find ./runtime -type f -exec chmod 0664 {} \;
+find ./runtime -type d -exec chmod 0775 {} \;
+
+mkdir -p ./storage \
+    ./storage/framework \
+    ./storage/framework/logs \
+    ./storage/framework/cache \
+    ./storage/framework/views \
+    ./storage/framework/sessions \
+;
+
+find ./storage -type f -exec chmod 0664 {} \;
+find ./storage -type d -exec chmod 0775 {} \;
+
+mkdir -p ./bootstrap/cache
+find ./bootstrap/cache -type f -exec chmod 0664 {} \;
+find ./bootstrap/cache -type d -exec chmod 0775 {} \;
+
+php artisan view:clear
+php artisan cache:clear
+php artisan config:clear
+rm ./bootstrap/cache/config.php || true
+php artisan config:cache
+
+php artisan route:clear
+php artisan route:cache
+
+set +xe
